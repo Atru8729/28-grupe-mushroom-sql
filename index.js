ec29e2bea1de251d3892467d8a3e87d9d15e3d79
Atru8729 <a.truskauskas1@gmail.com>
2021-09-04 13:10:24 +0300

    start

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ app.init = async () => {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        database: 'your_database_name',
+        database: 'mushroom',
     });
 
     let sql = '';
     let rows = [];
 
+    sql = 'SELECT * FROM `trips`';
+    [rows] = await connection.execute(sql);
+
     // LOGIC BELOW
+    // ** 1. ** _Isspausdinti, visu grybu pavadinimus ir ju kainas, grybus isrikiuojant nuo brangiausio link pigiausio_
+
+    //     pvz.:
+    //     ```
+    // Grybai:
+    // 1) Pavadinimas - 10.00 EUR/kg
+    // 2) Pavadinimas - 8.00 EUR/kg
+    // 3) Pavadinimas - 5.00 EUR/kg
+    // ```
 
 
 }
